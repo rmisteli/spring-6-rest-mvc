a59360a76c9c39b2dad2fe60f886bb215ed0415e
retomisteli <info@rmisteli.ch>
2024-11-14 14:35:00 +0100

    Assignment #16

---
+++ src/test/java/guru/springframework/spring6restmvc/controller/CustomerControllerIT.java
@@ -0,0 +1,59 @@
+package guru.springframework.spring6restmvc.controller;
+
+import guru.springframework.spring6restmvc.entity.Customer;
+import guru.springframework.spring6restmvc.model.CustomerDTO;
+import guru.springframework.spring6restmvc.repository.CustomerRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+class CustomerControllerIT {
+    @Autowired
+    CustomerController customerController;
+
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @Test
+    void testGetBeerByIdNotFound() {
+        assertThrows(NotFoundException.class, () -> {
+            customerController.getCustomerById(UUID.randomUUID());
+        });
+    }
+
+    @Test
+    void testGetById() {
+        Customer customer = customerRepository.findAll().get(0);
+
+        CustomerDTO customerDTO = customerController.getCustomerById(customer.getId());
+
+        assertThat(customerDTO).isNotNull();
+        assertThat(customerDTO.getId()).isEqualTo(customer.getId());
+    }
+
+    @Test
+    void testListCustomer() {
+        List<CustomerDTO> dtos = customerController.listCustomers();
+
+        assertThat(dtos.size()).isEqualTo(3);
+    }
+
+    @Rollback
+    @Transactional
+    @Test
+    void testEmptyList() {
+        customerRepository.deleteAll();
+        List<CustomerDTO> dtos = customerController.listCustomers();
+
+        assertThat(dtos.size()).isEqualTo(0);
+    }
+}
